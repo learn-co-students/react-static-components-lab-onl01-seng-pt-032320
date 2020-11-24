72c191480dfa1a6eab53096ade4f7708d27444c5
Latricia <tricianickelberry@gmail.com>
2020-11-24 09:27:18 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import React, {CatComponent} from 'react';
+import React, {GraceHopperQuoteComponent} from 'react';
 
 class App extends React.Component {
 	render() {
